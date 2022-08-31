a49deb8790eb0e6aaec772857748ee3a87f5c911
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-08-31 22:41:47 +0300

    Add reducer function in reducer.ts

--- AngularRedux/src/app/app.module.ts
+++ AngularRedux/src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// REDUX
+import { StoreModule } from '@ngrx/store';
+import { reducer } from '../reducer'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot( { message: reducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ AngularRedux/src/reducer.ts
@@ -0,0 +1,20 @@
+import { Action } from '@ngrx/store';
+
+// Takes the current state and copy it over the to new state
+// based on whatever changes the action is trying to make
+export function reducer(state: string = 'Hello World!', action: Action){
+  console.log(action.type, state);
+
+  // Application state is string so we can return brand new string
+  switch(action.type){
+    case 'Bulgarian':
+      return state = 'Здравей свят!';
+
+    case 'German':
+      return state = 'Hallo welt!';
+
+    default:
+      return state;
+  }
+
+}
